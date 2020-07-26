650e112c22373580ae0478fdf72cf08fd7ab0d92
Santiago-Torres14 <santiagoatorres14@gmail.com>
2020-07-25 23:32:28 -0500

    States and events

--- my-app/src/layouts/index.js
+++ my-app/src/layouts/index.js
@@ -1,9 +1,7 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Helmet from 'react-helmet'
-
-import Header from '../components/header'
-import './index.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Helmet from 'react-helmet';
+import './index.css';
 
 const Layout = ({ children, data }) => (
   <div>
@@ -14,17 +12,7 @@ const Layout = ({ children, data }) => (
         { name: 'keywords', content: 'sample, something' },
       ]}
     />
-    <Header siteTitle={data.site.siteMetadata.title} />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
       {children()}
-    </div>
   </div>
 )
 
